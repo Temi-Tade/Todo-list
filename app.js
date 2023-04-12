981d50761c22284e8f67804bfb7f7426a2e455d3
akintadetemi19@gmail.com <akintadetemi19@gmail.com>
2023-04-12 18:04:20 +0100

    Starting to fix ui, cleaning the JavaScript

--- app.js
+++ app.js
@@ -8,20 +8,6 @@ num = document.querySelector('#num')
 modbg = document.querySelector('#pop-up-bg')
 mod = document.querySelector("#pop-up")
 
-class Task{
-  constructor(name, status){
-    this.name = name;
-    this.status = 'pending';
-  } 
-  isCompleted(){
-    if (this.status === 'completed') {
-      return true
-    }else{
-      return false
-    }
-  }
-}
-
 function Message(msg,bg,color){
     message.innerHTML = msg;
     message.style.background = bg;
@@ -34,30 +20,19 @@ function Message(msg,bg,color){
   }
 }
 
-form.addEventListener('submit',savetask)
-
-function savetask(e){
-  e.preventDefault()
-  if (input.value.trim() === '' || input.value.trim() === undefined || input.value.trim() === null) {
-    let x = new Message('Error...Task cannot be empty','red','white')
-    x.display()
-   return false
-  }else{
-    let task = new Task(input.value.trim(), 'pending')
-    if (localStorage.getItem('tasks') === null) {
-      var taskArr = []
-      taskArr.push(task)
-      localStorage.setItem('tasks',JSON.stringify(taskArr))
-    } else {
-      var taskArr = JSON.parse(localStorage.getItem('tasks'))
-      taskArr.push(task)
-      localStorage.setItem('tasks',JSON.stringify(taskArr))
-    }
-  }
-  form.reset()
-  loadData()
-}
-
-function popUp(head, body){
+function popUpMessage(content){
 	modbg.style.display = 'block'
+	mod.innerHTML = content
 }
+
+document.querySelector('#addbtn').addEventListener('click', () => {
+	popUpMessage(`
+	<h3>Add a task</h3>
+	<form id="todo-form" autocomplete="off">
+		<div class="todo-input">
+	    	<input type="text" id="task" placeholder="Add a task..." autofocus>
+	    	<button type="submit" id="submit">add</button>
+	    </div>
+	</form>
+	`)
+})
